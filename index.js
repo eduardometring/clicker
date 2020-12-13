3ad4805df3b1b6cb46ad6f20f9fa6cda36dbf208
Ricardo Metring <ricardometring@gmail.com>
2020-12-12 22:33:14 -0300

    Intro + Bootstrap + JS

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+// // index.html
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/public/intro.html');
 });
+
+app.get('/game', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+// Arquivos estáticos
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
 	console.log('a user connected');
